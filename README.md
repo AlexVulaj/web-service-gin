4a3f11cf27ca4eff12744b7449b9b7de82013b1d
Alex Vulaj <avulaj@redhat.com>
2022-09-30 16:10:07 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-service-gin
+
+A sample REST API built following https://go.dev/doc/tutorial/web-service-gin.
---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+// album represents data about a record album.
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data.
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func main() {
+	router := gin.Default()
+	//router.GET("/albums", getAlbums)
+	router.GET("/albums", func(c *gin.Context) {
+		if c.Query("maxPrice") != "" {
+			getAlbumsMaxPrice(c)
+		} else {
+			getAlbums(c)
+		}
+	})
+	router.GET("/albums/:id", getAlbumByID)
+	router.POST("/albums", postAlbums)
+
+	err := router.Run("localhost:8080")
+	if err != nil {
+		println("Unable to start router.")
+		return
+	}
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
+// getAlbumsMaxPrice responds with the list of all albums with a price less than or equal to than the given param as JSON.
+func getAlbumsMaxPrice(c *gin.Context) {
+	maxPrice, err := strconv.ParseFloat(c.Query("maxPrice"), 64)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "query must be a number"})
+	}
+
+	var matches []album
+
+	// Loop over the list of albums, looking for
+	// albums whose price value is less than the parameter.
+	for _, a := range albums {
+		if a.Price <= maxPrice {
+			matches = append(matches, a)
+		}
+	}
+
+	if len(matches) > 0 {
+		c.IndentedJSON(http.StatusOK, matches)
+	} else {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "no matches found"})
+	}
+}
